f43890c0ce38d4d7bfac84b6bbee119823c23889
Ben <git@othyn.com>
2021-04-28 18:05:16 +0100

    Add test suite to the package, currently with ~95% test coverage

---
+++ tests/TestCase.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Support\Facades\Route;
+use jrmadsen67\LaravelRouteCoverageTest\Providers\CoverageServiceProvider;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Any service providers that the test may require.
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            CoverageServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Copies across the testing routes to the testing application.
+     */
+    protected function setupTestingRoutes(): void
+    {
+        Route::group([
+            'prefix'     => '_named_test',
+            'as'         => 'testing.',
+            'middleware' => [
+                'laravel-route-coverage-test',
+            ],
+        ], function () {
+            Route::name('one')
+                ->get('one', function () {
+                    return response()->noContent();
+                });
+
+            Route::name('two')
+                ->get('two', function () {
+                    return response()->noContent();
+                });
+        });
+
+        Route::group([
+            'prefix'     => '_test',
+            'middleware' => [
+                'laravel-route-coverage-test',
+            ],
+        ], function () {
+            Route::get('three', function () {
+                return response()->noContent();
+            });
+
+            Route::get('four', function () {
+                return response()->noContent();
+            });
+        });
+    }
+}
